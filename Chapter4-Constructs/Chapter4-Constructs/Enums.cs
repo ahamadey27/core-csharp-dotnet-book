d0e51ec1a2a0d8e512c1ce754e81bac5c5a2cbae
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-07-11 09:37:41 -0400

    Refactor Program.cs and add Enums class in Enums.cs
    Removed a null check method call in Program.cs and retained commented-out AddOperations calls. Added necessary using directives and created an Enums class with an EmpTypeEnum enumeration in Enums.cs.

---
+++ Chapter4-Constructs/Chapter4-Constructs/Enums.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter4_Constructs
+{
+    public class Enums
+    {
+        //Enums a custom data type of name-value pairs
+        enum EmpTypeEnum
+        {
+            Manger = 10,
+            Grunt = 1,
+            Contractor = 100,
+            Veep = 9
+        }
+       
+
+
+
+    }
+}
