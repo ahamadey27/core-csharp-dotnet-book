527f914a584c2304b51f0146096b152fbd02c7e5
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-07-11 10:01:58 -0400

    Will visit structs later

---
+++ Chapter4-Constructs/Chapter4-Constructs/Structures.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter4_Constructs
+{
+    public class Structures
+    {
+        public struct Point
+        {
+            //Fields of structs
+            
+
+            //Add 1 to the xy position
+            public static void Increment()
+            {
+                int x;
+                int y;
+               
+            }
+        }
+    }
+}
